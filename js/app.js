110aa3585f87870ac03efba4e9e8bfd60498d14d
Kush Shrestha <kush_shrestha01@yahoo.com>
2019-03-27 17:47:58 -0700

    Lab 3 completed

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 'use strict';
-//if (userName !== null){
-//Q1
+//Current Date
+var d = new Date();
+document.getElementById('date').innerHTML = d;
+
+var correctAnswer = 0;
+var totalQuestions = 5;
+
+//User Name
 var userName = prompt('What is your name?');
 alert('Hi, ' + userName + ', great to meet you!!!. Lets play a guessing game..');
 console.log('When asked their name, the user responded: ' + userName);
@@ -8,17 +14,18 @@ console.log('When asked their name, the user responded: ' + userName);
 var nameResult = 'Great to meet you ' + userName.toUpperCase() ;
 document.getElementById('output1').innerHTML = nameResult;
 
-//Q2
-var userSports = prompt('Do you play sports');
+//Q1
+var userSports = prompt('Do I play sports');
 var uppercaseSports = userSports.toUpperCase();
 var resultSports;
 if(uppercaseSports === 'Y' || uppercaseSports ==='YES'){
-  alert('I play sports too!!!');
-  resultSports = 'I play sport too ';
+  alert('Correct');
+  resultSports = 'I play sport.';
   document.getElementById('output2').innerHTML = resultSports;
+  correctAnswer++;
 }
 else if (uppercaseSports === 'N' || uppercaseSports ==='NO'){
-  alert ('okay');
+  alert ('Incorrect');
   resultSports = 'I like sports. Sports are fun, you should try it';
   document.getElementById('output2').innerHTML = resultSports;
 }
@@ -27,17 +34,18 @@ else{
 }
 console.log('When asked if they play sports user responded ' + uppercaseSports);
 
-//Q3
-var userTravel = prompt('Do you like to travel?');
+//Q2
+var userTravel = prompt('Do I like to travel?');
 var uppercaseTravel = userTravel.toUpperCase();
 var resultTravel;
 if(uppercaseTravel === 'Y' || uppercaseTravel ==='YES'){
-  alert('Thats good!!!');
-  resultTravel = 'I like to travel as well';
+  alert('Correct');
+  resultTravel = 'Travelling is my hobby';
   document.getElementById('output3').innerHTML = resultTravel;
+  correctAnswer++;
 }
 else if (uppercaseTravel === 'N' || uppercaseTravel ==='NO'){
-  alert ('okay');
+  alert ('Incorrect');
   resultTravel = 'I like to travel, it can be fun';
   document.getElementById('output3').innerHTML = resultTravel;
 }
@@ -46,36 +54,38 @@ else{
 }
 console.log('When asked if user like to travel: ' + uppercaseTravel);
 
-//Q4
+//Q3
 var userMovies = prompt('Do you like to Horror Movies?');
 var uppercaseMovies = userMovies.toUpperCase();
 var resultMovies;
 if(uppercaseMovies === 'Y' || uppercaseMovies ==='YES'){
-  alert('Okay');
-  resultMovies = 'I enjoy horror movies too';
+  alert('Incorrect');
+  resultMovies = 'I do not like horror movies';
   document.getElementById('output4').innerHTML = resultMovies;
 }
 else if (uppercaseMovies === 'N' || uppercaseMovies === 'NO'){
-  alert ('Okay');
+  alert ('Correct');
   resultMovies = 'I enjoy horror movies, but it is not for everybody';
   document.getElementById('output4').innerHTML = resultMovies;
+  correctAnswer++;
 }
 else{
   alert ('please refresh and type yes/y or no/n');
 }
 console.log('When asked if user like Horror Movies: ' + uppercaseMovies);
 
-//Q5
-var userMusic = prompt('Do you like to pop music?');
+//Q4
+var userMusic = prompt('Do I like to pop music?');
 var uppercaseMusic = userMusic.toUpperCase();
 var resultMusic;
 if(uppercaseMusic === 'Y' || uppercaseMusic ==='YES'){
-  alert('Okay');
-  resultMusic = 'I enjoy pop music as well';
+  alert('Correct');
+  resultMusic = 'I enjoy pop music.';
   document.getElementById('output5').innerHTML = resultMusic;
+  correctAnswer++;
 }
 else if (uppercaseMusic === 'N' || uppercaseMusic === 'NO'){
-  alert ('Uhm...');
+  alert ('Uhm... Incorrect');
   resultMusic = 'I like pop music.';
   document.getElementById('output5').innerHTML = resultMusic;
 }
@@ -84,25 +94,64 @@ else{
 }
 console.log('When asked if user like pop music: ' + uppercaseMusic);
 
-//Q6
-var userPets = prompt('Do you like to like pets?');
+//Q5
+var userPets = prompt('Is dog my favorite pet?');
 var uppercasePets = userPets.toUpperCase();
 var resultPets;
 if(uppercasePets === 'Y' || uppercasePets ==='YES'){
-  alert('Okay');
-  resultPets = 'Me too. I love dogs and cats';
+  alert('Correct');
+  resultPets = 'I love dogs and cats';
   document.getElementById('output6').innerHTML = resultPets;
+  correctAnswer++;
 }
 else if (uppercasePets === 'N' || uppercasePets === 'NO'){
-  alert ('Uhm...');
-  resultPets = 'I love pets though';
+  alert ('Incorrect');
+  resultPets = 'I love dogs and cats';
   document.getElementById('output6').innerHTML = resultPets;
 }
 else{
   alert ('please refresh and type yes/y or no/n');
 }
-console.log('When asked if user like pop music: ' + uppercaseMusic);
+console.log('When asked if user like pets: ' + uppercasePets);
 
-var d = new Date();
-document.getElementById('date').innerHTML = d;
-//}
+//Q6
+var guessNumber;
+guessNumber = prompt ('Lets...Guess my favorite number');
+var i = 0;
+for(i = 0; i<5; i++){
+  if (guessNumber === '3'){
+    alert ('You are right');
+    break;
+  }
+  else if (guessNumber>3){
+    alert ('guess lower');
+    guessNumber = prompt ('Guess my favorite number');
+  }
+  else if (guessNumber<3){
+    alert ('guess higher');
+    guessNumber = prompt ('Guess my favorite number');
+  }
+}
+
+//Q7
+var livedStates = ['TEXAS','MINESOTTA','INDIANA','HAWAII'];
+var guessState;
+var uppercaseState;
+for(var j = 0; j < 5; j++){
+  guessState = prompt('Can you guess a state that I have lived in besides Washington?');
+  uppercaseState = guessState.toUpperCase();
+  console.log(livedStates.includes(uppercaseState));
+
+  if (livedStates.includes(uppercaseState)){
+    alert ('You are right. I have lived in Texas, Minnesota, Indiana, Hawaii.');
+    console.log(uppercaseState);
+    break;
+  }
+  else{
+    alert ('try again');
+  }
+}
+
+//Quiz Results
+var totalResults = 'You got ' + correctAnswer + ' correct out of ' + totalQuestions + '.';
+document.getElementById('output7').innerHTML = totalResults;
