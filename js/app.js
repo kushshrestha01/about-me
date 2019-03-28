4c1f1f6b633593ce9f0df91a88a3411279965e42
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2019-03-28 16:54:07 -0700

    refactor yes or no guessing game to a function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,51 @@
 'use strict';
+
+function playYesNoGame() {
+  var validResponses = ['YES', 'NO'];
+
+  var allQuestions = ['Do I play sports',
+    'Do I like to travel?',
+    'Do you like to Horror Movies?',
+    'Do I like to pop music?',
+    'Is dog my favorite pet?'];
+
+  var allCorrectAnswers = ['YES', 'YES', 'NO', 'YES', 'YES'];
+
+  var outputForCorrectAnswers = [ 'I play sport.',
+    'Travelling is my hobby',
+    'I do not like horror movies',
+    'I enjoy pop music.',
+    'I love dogs and cats'
+  ];
+
+  var outputForIncorrectAnswers = ['I like sports. Sports are fun, you should try it',
+    'I like to travel, it can be fun',
+    'I enjoy horror movies, but it is not for everybody',
+    'I like pop music.',
+    'I love dogs and cats'
+  ];
+
+  for (var i = 0; i < allQuestions.length; i++) {
+    //prompt user for question
+    var answer  = prompt(allQuestions[i]).toUpperCase();
+
+    //check to see if  user input is yes, y, no, n
+    while (!validResponses.includes(answer)) {
+      alert ('please type yes or no');
+      answer = prompt(allQuestions[i]).toUpperCase();
+    } 
+
+    //if answer is correct  alert correct or not, and add 1 to correct answer
+    if (answer === allCorrectAnswers[i]) {
+      alert('Correct');
+      correctAnswer++;
+      document.getElementById('output' + (i + 2)).innerHTML = outputForCorrectAnswers[i];
+    } else {
+      alert('Incorrect');
+      document.getElementById('output' + (i + 2)).innerHTML = outputForIncorrectAnswers[i];
+    }
+  }
+}
 //Current Date
 var d = new Date();
 document.getElementById('date').innerHTML = d;
@@ -14,109 +61,13 @@ console.log('When asked their name, the user responded: ' + userName);
 var nameResult = 'Great to meet you ' + userName.toUpperCase() ;
 document.getElementById('output1').innerHTML = nameResult;
 
-//Q1
-var userSports = prompt('Do I play sports');
-var uppercaseSports = userSports.toUpperCase();
-var resultSports;
-if(uppercaseSports === 'Y' || uppercaseSports ==='YES'){
-  alert('Correct');
-  resultSports = 'I play sport.';
-  document.getElementById('output2').innerHTML = resultSports;
-  correctAnswer++;
-}
-else if (uppercaseSports === 'N' || uppercaseSports ==='NO'){
-  alert ('Incorrect');
-  resultSports = 'I like sports. Sports are fun, you should try it';
-  document.getElementById('output2').innerHTML = resultSports;
-}
-else{
-  alert ('please refresh and type yes/y or no/n');
-}
-console.log('When asked if they play sports user responded ' + uppercaseSports);
-
-//Q2
-var userTravel = prompt('Do I like to travel?');
-var uppercaseTravel = userTravel.toUpperCase();
-var resultTravel;
-if(uppercaseTravel === 'Y' || uppercaseTravel ==='YES'){
-  alert('Correct');
-  resultTravel = 'Travelling is my hobby';
-  document.getElementById('output3').innerHTML = resultTravel;
-  correctAnswer++;
-}
-else if (uppercaseTravel === 'N' || uppercaseTravel ==='NO'){
-  alert ('Incorrect');
-  resultTravel = 'I like to travel, it can be fun';
-  document.getElementById('output3').innerHTML = resultTravel;
-}
-else{
-  alert ('please refresh and type yes/y or no/n');
-}
-console.log('When asked if user like to travel: ' + uppercaseTravel);
-
-//Q3
-var userMovies = prompt('Do you like to Horror Movies?');
-var uppercaseMovies = userMovies.toUpperCase();
-var resultMovies;
-if(uppercaseMovies === 'Y' || uppercaseMovies ==='YES'){
-  alert('Incorrect');
-  resultMovies = 'I do not like horror movies';
-  document.getElementById('output4').innerHTML = resultMovies;
-}
-else if (uppercaseMovies === 'N' || uppercaseMovies === 'NO'){
-  alert ('Correct');
-  resultMovies = 'I enjoy horror movies, but it is not for everybody';
-  document.getElementById('output4').innerHTML = resultMovies;
-  correctAnswer++;
-}
-else{
-  alert ('please refresh and type yes/y or no/n');
-}
-console.log('When asked if user like Horror Movies: ' + uppercaseMovies);
-
-//Q4
-var userMusic = prompt('Do I like to pop music?');
-var uppercaseMusic = userMusic.toUpperCase();
-var resultMusic;
-if(uppercaseMusic === 'Y' || uppercaseMusic ==='YES'){
-  alert('Correct');
-  resultMusic = 'I enjoy pop music.';
-  document.getElementById('output5').innerHTML = resultMusic;
-  correctAnswer++;
-}
-else if (uppercaseMusic === 'N' || uppercaseMusic === 'NO'){
-  alert ('Uhm... Incorrect');
-  resultMusic = 'I like pop music.';
-  document.getElementById('output5').innerHTML = resultMusic;
-}
-else{
-  alert ('please refresh and type yes/y or no/n');
-}
-console.log('When asked if user like pop music: ' + uppercaseMusic);
-
-//Q5
-var userPets = prompt('Is dog my favorite pet?');
-var uppercasePets = userPets.toUpperCase();
-var resultPets;
-if(uppercasePets === 'Y' || uppercasePets ==='YES'){
-  alert('Correct');
-  resultPets = 'I love dogs and cats';
-  document.getElementById('output6').innerHTML = resultPets;
-  correctAnswer++;
-}
-else if (uppercasePets === 'N' || uppercasePets === 'NO'){
-  alert ('Incorrect');
-  resultPets = 'I love dogs and cats';
-  document.getElementById('output6').innerHTML = resultPets;
-}
-else{
-  alert ('please refresh and type yes/y or no/n');
-}
-console.log('When asked if user like pets: ' + uppercasePets);
+//play yes or no game
+playYesNoGame();
 
 //Q6
 var guessNumber;
-guessNumber = prompt ('Lets...Guess my favorite number');
+guessNumber = prompt ('Guess my favorite number');
+
 var i = 0;
 for(i = 0; i<5; i++){
   if (guessNumber === '3'){
